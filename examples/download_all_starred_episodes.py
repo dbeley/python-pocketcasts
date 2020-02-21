e9b84fc9eb177f558f88971c67298b7512279e87
dbeley <6568955+dbeley@users.noreply.github.com>
2020-02-21 23:00:24 +0100

    Update download_all_starred_episodes.

--- examples/download_all_starred_episodes.py
+++ examples/download_all_starred_episodes.py
@@ -6,16 +6,8 @@ from pathlib import Path
 
 
 def get_valid_filename(s):
-    """
-    Return the given string converted to a string that can be used for a clean
-    filename. Remove leading and trailing spaces; convert other spaces to
-    underscores; and remove anything that is not an alphanumeric, dash,
-    underscore, or dot.
-    >>> get_valid_filename("john's portrait in 2004.jpg")
-    'johns_portrait_in_2004.jpg'
-    """
-    s = str(s).strip().replace(" ", "_")
-    return re.sub(r"(?u)[^-\w.]", "", s)
+    s = str(s).strip()
+    return re.sub(r"(?u)[^-\w. ]", "", s)
 
 
 print("Reading configuration file config.ini.")
@@ -33,11 +25,11 @@ print("Downloading starred episodes.")
 total_size = len(starred)
 
 Path("podcasts/").mkdir(parents=True, exist_ok=True)
-for index, i in enumerate(starred):
+for index, i in enumerate(starred, 1):
     filename = get_valid_filename(
-        f"{i._published_at.split('_')[-1]} - {i._podcast._title} - {i._title}.{i._url.split('.')[-1]}"
+        f"{i._published_at.strftime('%Y%m%d')} - {i._podcast._title} - {i._title}.{i._url.split('.')[-1]}"
     )
-    print(f"{index}/{total_size} : {filename}")
+    print(f"Downloading {index}/{total_size} : {filename}")
     r = requests.get(i._url)
     with open("podcasts/" + filename, "wb") as f:
         f.write(r.content)
